f76ac53c2db08b373ed23b05b37fafb57ec763c0
anirudhburman <anirudh.burman.micro@gmail.com>
2023-01-27 17:39:25 +0530

    Made all packages and Initial Commit

---
+++ src/main/java/com/capgemini/go/GreatoutdoorsApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatoutdoorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatoutdoorsApplication.class, args);
+	}
+
+}
